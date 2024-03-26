159ee9a18c2bdec0c6a78d9b79f51288bfde5a6c
Diptanil Saha <diptanil.dev@gmail.com>
2024-03-27 01:24:53 +0530

    feat: non admin user password verification and setting application details

--- proctor_client/main.py
+++ proctor_client/main.py
@@ -7,6 +7,7 @@ from PyQt6.QtWidgets import QApplication, QMessageBox
 from proctor_client.utils.database import LocalDatabase
 from proctor_client.utils.rest_api import ProctorRestAPI
 from proctor_client.admin_dialogs.admin import AdminMainWindow
+from proctor_client.user_dialogs.password import VerifyPasswordDialog
 from proctor_client.user_dialogs.tray import ProctorClientSystemTray
 from proctor_client.utils.admin import isAdmin
 from proctor_client.constants import WINDOW_TITLE, ICON, APP_DIR, LOCAL_DB
@@ -14,6 +15,10 @@ from proctor_client.constants import WINDOW_TITLE, ICON, APP_DIR, LOCAL_DB
 class ProctorClientApp(QApplication):
     def __init__(self, argv: List[str]) -> None:
         super().__init__(argv)
+
+        self.setApplicationName('Proctor Client')
+        self.setApplicationDisplayName('Proctor Client')
+        self.setApplicationVersion('v1.1')
 
         if not os.path.exists(APP_DIR):
             os.mkdir(APP_DIR)
@@ -44,6 +49,18 @@ class ProctorClientApp(QApplication):
                 dlg.setIcon(QMessageBox.Icon.Critical)
                 dlg.exec()
                 sys.exit(0)
+
+            vpd = VerifyPasswordDialog(password_hash)
+            if not vpd.exec():
+                dlg = QMessageBox()
+                dlg.setWindowIcon(QIcon(ICON))
+                dlg.setWindowTitle(WINDOW_TITLE)
+                dlg.setText("Proctor Client could not start.")
+                dlg.setStandardButtons(QMessageBox.StandardButton.Ok)
+                dlg.setIcon(QMessageBox.Icon.Critical)
+                dlg.exec()
+                sys.exit(0)
+
             self.setQuitOnLastWindowClosed(False)
             self.tray = ProctorClientSystemTray(
                 self.db,
